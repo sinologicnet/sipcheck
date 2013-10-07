335f96975edabeb371b09acb9cfab52455812b3f
root <root@bookmarker.es>
2013-10-07 19:36:17 +0200

    modified:   client.py - Start of adaptation to new protocol (TEST)

--- client.py
+++ client.py
@@ -2,15 +2,6 @@
 # -*- coding: utf-8 -*-
 """
     Testing control connection with server.
-    Simple protocol:
-        -login: 
-             -send user:password
-             -get "ok" from server to continue
-
-        -wait for commands:
-             -add ban x.x.x.x
-             -del ban x.x.x.x
-             -...
              
 """
 import socket
@@ -20,14 +11,29 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
+server_address = ('www.sinologic.net', 6969)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 try:
+
+    # HELO
+    message = "HELO\r\n"
+    print >>sys.stderr, 'HELO on %s port %s' % server_address
+    sock.sendall(message)
+
+    # Wait for HELO confirmation and code
+    data = sock.recv(1024)
+    data = data.replace('\r', '')
+    data = data.replace('\n', '')
+    print >>sys.stderr, 'received "%s"' % data
+
+    code = ""
+    if "HELO" in data:
+        code = data.split(" ")[1]
     
     # Send login 
-    message = 'user:pass\r\n'
+    message = 'USER '+code+' usuario\r\nPASS '+code+' clave\r\n'
     print >>sys.stderr, 'login on %s port %s' % server_address
     sock.sendall(message)
 
@@ -37,7 +43,7 @@ try:
     data = data.replace('\n', '')
     print >>sys.stderr, 'received "%s"' % data
 
-    if data == "ok" :
+    if "OK" in data :
         print >>sys.stderr, 'logged in'
         while True:
             data = sock.recv(1024)
