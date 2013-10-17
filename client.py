ad9e691c2b1d8d740e894906e04bc7bd3d7ab278
root <root@bookmarker.es>
2013-10-17 19:24:09 +0200

    srv.py    PoC server side protocol

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('www.sinologic.net', 6969)
+server_address = ('localhost', 6969)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
