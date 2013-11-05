ac882808410d0d9647aa772823c50516c1b73162
Seattle113 <113.seattle@gmail.com>
2013-11-05 22:11:25 +0100

    config object

---
+++ sipcheck/config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+    Read & parse config file
+'''
+
+import ConfigParser
+
+class Config(object):
+    ''' Read & parse config file '''
+
+    def __init__(self, config_file):
+        ''' consutructor '''
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(config_file)
+
+    def get_general(self, key):
+        ''' Return value of general config '''
+        return self.config.get('general', key)
+
+    def get_sahred(self, key):
+        ''' Return value of sahred config '''
+        return self.config.get('sahred', key)
+
+    def get_database(self, key):
+        ''' Return value of database config '''
+        return self.config.get('database', key)
