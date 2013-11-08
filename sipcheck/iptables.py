84cede11ca3920ff43b39c78c7a082472952f9ab
Seattle113 <113.seattle@gmail.com>
2013-11-08 23:36:54 +0100

    block ip

--- sipcheck/iptables.py
+++ sipcheck/iptables.py
@@ -9,22 +9,19 @@ class IPTables(object):
     ''' IPTables wrapper '''
     dbconn = None
 
-    def __init__(self, dbconn):
+    def __init__(self):
         ''' Constructor '''
-        self.dbconn = dbconn
         self.logger = logging.getLogger('sipcheck')
 
     def block(self, ipaddress):
         ''' Set an IP adress as blocked an blocks with iptables '''
         self.logger.debug('Blocking %s IP address' % ipaddress)
-        self.dbconn.block(ipaddress)
         os.system ("iptables -A INPUT -s %s -j DROP" % ipaddress)
         return True
 
     def unblock(self, ipaddress):
         ''' Unset an IP as bloked an release iptables block '''
         self.logger.debug('Unblocking %s IP address' % ipaddress)
-        self.dbconn.block(ipaddress)
         os.system ("iptables -D INPUT -s %s -j DROP" % ipaddress)
         return True
 
