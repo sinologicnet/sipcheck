34db490556852b06c34bc42da9d40aca4767b083
Seattle113 <113.seattle@gmail.com>
2013-11-09 19:17:45 +0100

    version 2.0.0-alpha1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='sipcheck',
-      version='2.0.0',
+      version='2.0.0-alpha1',
       description='',
       url='https://github.com/sinologicnet/sipcheck',
       author='Elio Rojano, Sergio Cotelo, Javier Vidal',
